a5b0af8cfadcce3e3fd9eb423cc038e8598a769f
edebach <edebach@yahoo.it>
2023-04-28 19:47:09 +0200

    profilo da terminare

--- Logged/Profilo.php
+++ Logged/Profilo.php
@@ -38,62 +38,106 @@ $dbconn = pg_connect("host=localhost port=5432 dbname=Scholarnet
     </head>
     <body>
 
-        <section class="vh-100" style="background-color: #f4f5f7;">
-            <div class="container py-5 h-100">
-              <div class="row d-flex justify-content-center align-items-center h-100">
-                <div class="col col-lg-6 mb-4 mb-lg-0">
-                  <div class="card mb-3" style="border-radius: .5rem;">
-                    <div class="row g-0">
-                        <button type="button" class="btn-close" aria-label="Close" onclick="window.history.back()"></button>
-                      <div class="col-md-4 gradient-custom text-center text-white"
-                        style="border-top-left-radius: .5rem; border-bottom-left-radius: .5rem;">
-                        <!-- immagine profilo in base a professione e sesso -->
-                          <?php
-                          include "./Profilo/fotoprofilo.php"
-                          ?>
-                          <?php
-                          include "./Profilo/nome_studprof.php"
-                          ?>
-                        <!-- <i class="far fa-edit mb-5"></i> -->
-                      </div>
-                      <div class="col-md-8">
-                        <div class="card-body p-4">
-                          <h6>Informazioni</h6>
-                          <hr class="mt-0 mb-4">
-                          <div class="row pt-1">
-                            <div class="col-6 mb-3">
-                              <h6>Email</h6>
-                              <p class="text-muted">info@example.com</p>
-                            </div>
-                            <div class="col-6 mb-3">
-                              <h6>Phone</h6>
-                              <p class="text-muted">123 456 789</p>
-                            </div>
-                          </div>
-                          <h6>Projects</h6>
-                          <hr class="mt-0 mb-4">
-                          <div class="row pt-1">
-                            <div class="col-6 mb-3">
-                              <h6>Recent</h6>
-                              <p class="text-muted">Lorem ipsum</p>
-                            </div>
-                            <div class="col-6 mb-3">
-                              <h6>Most Viewed</h6>
-                              <p class="text-muted">Dolor sit amet</p>
-                            </div>
-                          </div>
-                          <div class="d-flex justify-content-start">
-                            <a href="#!"><i class="fab fa-facebook-f fa-lg me-3"></i></a>
-                            <a href="#!"><i class="fab fa-twitter fa-lg me-3"></i></a>
-                            <a href="#!"><i class="fab fa-instagram fa-lg"></i></a>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
+    <section class="vh-100" style="background-color: #f4f5f7;">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col col-lg-6 mb-4 mb-lg-0">
+        <div class="card mb-3" style="border-radius: .5rem;">
+          <div class="row g-0">
+            <button type="button" class="btn-close" aria-label="Close" onclick="window.history.back()"></button>
+            <div class="col-md-4 gradient-custom text-center text-white" style="border-top-left-radius: .5rem; border-bottom-left-radius: .5rem;">
+              <!-- immagine profilo in base a professione e sesso -->
+              <?php include "./Profilo/fotoprofilo.php" ?>
+              <?php include "./Profilo/nome_studprof.php" ?>
+              <!-- <i class="far fa-edit mb-5"></i> -->
+            </div>
+            <div class="col-md-8">
+              <div class="card-body p-4">
+                <h6>Informazioni</h6>
+                <hr class="mt-0 mb-4">
+                <div class="row pt-1">
+                  <div class="col-6 mb-3">
+                    <h6>Email</h6>
+                    <p class="text-muted">
+                      <span class="editable" id="email"><?php echo $_SESSION["email"];?></span>
+                      <input type="email" class="form-control d-none" id="email-input" value="<?php echo $_SESSION["email"];?>">
+                    </p>
                   </div>
+                  <div class="col-6 mb-3">
+                    <!-- TODO: numero di telefono da inserire da interfaccia utente editabile più volte -->
+                    <h6>Phone</h6>
+                    <p class="text-muted">
+                      <span class="editable" id="phone">123 456 789</span>
+                      <input type="tel" class="form-control d-none" id="phone-input" value="123 456 789">
+                    </p>
+                  </div>
+                </div>
+                <h6>Projects</h6>
+                <hr class="mt-0 mb-4">
+                <div class="row pt-1">
+                  <div class="col-6 mb-3">
+                    <h6>Recent</h6>
+                    <p class="text-muted">Lorem ipsum</p>
+                  </div>
+                  <div class="col-6 mb-3">
+                    <h6>Most Viewed</h6>
+                    <p class="text-muted">Dolor sit amet</p>
+                  </div>
+                </div>
+                <div class="d-flex justify-content-start">
+                  <a href="#!"><i class="fab fa-facebook-f fa-lg me-3"></i></a>
+                  <a href="#!"><i class="fab fa-twitter fa-lg me-3"></i></a>
+                  <a href="#!"><i class="fab fa-instagram fa-lg"></i></a>
+                </div>
+                <div class="d-flex justify-content-end mt-4">
+                  <button type="button" class="btn btn-primary me-2" id="edit-profile-btn">Modifica profilo</button>
+                  <button type="button" class="btn btn-success d-none" id="save-profile-btn">Salva modifiche</button>
                 </div>
               </div>
             </div>
-          </section>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+  $(document).ready(function() {
+    // Gestisci il clic sul pulsante "Modifica profilo"
+    $('#edit-profile-btn').on('click', function() {
+      $('.editable').addClass('d-none');
+      $('.form-control').removeClass('d-none');
+      $('#edit-profile-btn').addClass('d-none');
+      $('#save-profile-btn').removeClass('d-none');
+    });
+
+    // Gestisci il clic sul pulsante "Salva modifiche"
+    $('#save-profile-btn').on('click', function() {
+      $('.editable').removeClass('d-none');
+      $('.form-control').addClass('d-none');
+      $('#edit-profile-btn').removeClass('d-none');
+      $('#save-profile-btn').addClass('d-none');
+
+      // Aggiorna le informazioni nel database tramite una chiamata AJAX
+      $.ajax({
+        url: 'update_profile.php',
+        type: 'POST',
+        data: { // passa i dati aggiornati tramite POST
+          email: $('#email-input').val(),
+          phone: $('#phone-input').val()
+        },
+        success: function(response) {
+          // gestisci la risposta del server
+          console.log(response);
+        },
+        error: function(xhr, status, error) {
+          console.log(xhr.responseText);
+        }
+      });
+    });
+  });
+</script>
     </body>
 </html>
